ffd1f394e3241bd89aea874c3abc687e2bd1c26b
iqrazartasha33 <iqrazartasha33@gmail.com>
2024-10-04 02:15:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//Ans 1
+var answer = confirm("Do you like our website?");
+if(answer){
+    alert("Thank You!");
+} else{
+    alert("Sorry!");
+}
+
+//Ans 2
+var answer = prompt("What is the capital of Pakistan?")
+if(answer === "Islamabad"){
+    alert("Correct!");
+} else{
+    alert("Try again!")
+}
